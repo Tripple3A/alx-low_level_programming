8e3abef0673c20ce65a7713ff48d9d5455aa9002
Tripple3A <anne.alwala@ashesi.edu.gh>
2022-10-14 19:29:17 -0700

    my first c program using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - print to screen
- *
- * Return: zero
- */
+   * main - print to screen
+   *
+   * Return: zero
+   */
 
 int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	
+	
 	puts(Mystr);
 	return (0);
-}	
+
+ }	
 
