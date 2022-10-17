681e1027a0fb343fd798c686cf6ae2aff12ae5b3
Tripple3A <anne.alwala@ashesi.edu.gh>
2022-10-17 11:02:36 -0700

    generating random numbers

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  *main -prints alphabet in lower case
+  *Return zero
+  */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a' ; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+
+}
