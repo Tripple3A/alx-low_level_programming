b549b830328a2dc2e2a25006d185e075c136807b
Tripple3A <anne.alwala@ashesi.edu.gh>
2022-10-18 16:43:26 -0700

    printing printchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  *main - entry
+  *
+  *The code prints alphabet in upper and lower cas
+  *Return zero
+  */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	
+	putchar('\n');
+	return (0);
+}
