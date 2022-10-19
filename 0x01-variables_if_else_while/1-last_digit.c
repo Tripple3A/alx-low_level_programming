8ab91b34f3df9acb07b54df515c68c23d5242640
Tripple3A <anne.alwala@ashesi.edu.gh>
2022-10-19 14:03:54 -0700

    printing last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
-  *main-enty
+  *main-entry
   *Return: Always zero
   */
 int main(void)
@@ -17,13 +17,13 @@ int main(void)
 
 	if (a > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5/n", n, a);
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	}
 	else if (a == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, a);
 	}
-	else
+	else if (a < 6 && a != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  *times_table - print the 9 times table, starting with zero.
+  *
+  */
+void times_table(void)
+{
+	int n;
+	int i;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0');
+		or (i = 1; i <= 9; n++)
+		
+			int result = n * i;
+
+			_putchar(' ');
+			_putchar(' ');
+			(result <= 9) ?
+			_putchar(' '): (result / 10));
+			_putchar('0') - (result % 10));
+	_putchar('\n');
+}
+
+
