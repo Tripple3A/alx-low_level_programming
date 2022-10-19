6fdcbc357db751a59485540eb05514584503dbac
Tripple3A <anne.alwala@ashesi.edu.gh>
2022-10-18 18:13:15 -0700

    printing alphabets ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints ten times alphabet
+ *
+ *Return: zero
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
