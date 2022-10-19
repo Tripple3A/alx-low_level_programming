2e42257fd51bf249edfd69fe8b1cc03e3232b560
Tripple3A <anne.alwala@ashesi.edu.gh>
2022-10-19 08:19:48 -0700

    add absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *_abs- compute the absolute value
+  *@n: is to be checked
+  *Return:Always zero
+  */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		n = (-1) * n;
+	}
+	return (n);
+}
