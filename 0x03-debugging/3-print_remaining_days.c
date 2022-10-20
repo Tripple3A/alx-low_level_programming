6d86755d5779541ee459d7626847d05662a3bb5e
Tripple3A <anne.alwala@ashesi.edu.gh>
2022-10-20 16:28:05 -0700

    print remaining days of the year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -7,7 +7,7 @@
   *Descritpion: prints the date
   *@month: month in number format
   *@day: day of month
-  *year: year
+  *@year: year
   *Return: void
   */
 void print_remaining_days(int month, int day, int year)
