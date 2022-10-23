f84d9c7c943c000ea5f296eebf0c60232ddd4052
Tripple3A <anne.alwala@ashesi.edu.gh>
2022-10-23 15:10:54 -0700

    drawing a triangle on the terminal

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * print_triangle - printing a triangle on the terminal
+  *@size: size of the triangle
+  *Return: zero
+  */
+void print_triangle(int size)
+{
+	int i = 1, ii;
+
+	while (i <= size && size > 0)
+	{
+		ii = 0;
+		while (ii < size - i)
+		{
+			_putchar(' ');
+			ii++;
+		}
+		ii = 0;
+		while (ii < i)
+		{
+			_putchar('#');
+			ii++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+	{
+		_putchar('\n');
+	}
+}
