77493769c3f0b998faace67bdc9eb09182ad0932
Tripple3A <anne.alwala@ashesi.edu.gh>
2022-10-23 10:54:04 -0700

    checking if number is between zero and nine

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *_isdigit - checks if the number is in 0 to 9
+  *@c: number to be checkes
+  *Return: one or zero
+  */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
