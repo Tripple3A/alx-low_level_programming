5ed4bcd286a41bb6c34605dc1ccb90bf5d2ebf2b
Tripple3A <anne.alwala@ashesi.edu.gh>
2022-10-25 15:37:48 -0700

    printing to the standard output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *_puts - prints a string
+  *@str: a string
+  *Return: nothig
+  */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
