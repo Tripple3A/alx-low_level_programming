4d85281d9b99ec671ac6c4600ba11c6f7a797d21
Tripple3A <anne.alwala@ashesi.edu.gh>
2022-10-25 16:14:08 -0700

    printing the characters of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *puts2 - prints characters of a string
+  *@str: string
+  *Return: nothing
+  */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
