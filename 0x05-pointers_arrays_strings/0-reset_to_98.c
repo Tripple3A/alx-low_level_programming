b8258d854e7de53fd853c28d3a1ebd581e5c0e78
Tripple3A <anne.alwala@ashesi.edu.gh>
2022-10-25 14:52:28 -0700

    changing the value the pointer is pointing to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  *reset_to_98 - changes the value to 98
+  *@n: pointer to integer
+  *Return: 0
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Learning pointers and arrays
