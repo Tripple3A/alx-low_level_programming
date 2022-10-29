7aa5093f3df8cc68f723881946da8c6c466c41f6
Tripple3A <anne.alwala@ashesi.edu.gh>
2022-10-28 18:21:22 -0700

    encodes a string to 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,7 +8,7 @@
 char *leet(char *str)
 {
 	int indx1 = 0, indx2;
-	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[indx1])
 	{
